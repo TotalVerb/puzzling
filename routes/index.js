9ce32c68cea80c63a2b717fe9499673cc5a58db4
Fengyang Wang <fengyang.wang.0@gmail.com>
2015-09-19 17:52:34 -0400

    Admin panel

--- routes/index.js
+++ routes/index.js
@@ -30,4 +30,25 @@ router.post('/submit', function(req, res, next) {
     }
 });
 
+router.get('/stats', function(req, res, next) {
+  var stats = {};
+  var keys = db.keys();
+  for (var i = 0; i < keys.length; i++) {
+    var data = db.get(keys[i]);
+    var correct = {};
+    var score = 0;
+    for (var j = 1; j <= 7; j++) {
+      correct[j] = data[j] === ANSWERS[j];
+      score += correct[j];
+    }
+    stats[keys[i]] = {
+      time: data.last_submission,
+      answers: data,
+      correct: correct,
+      score: score
+    };
+  }
+  res.render('admin', {stats: stats});
+});
+
 module.exports = router;
